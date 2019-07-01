ab4ccc5f91962aac654e9f4ba8d8b1fd9f6a3ec6
孟宣 陳 <stezen129@hotmail.com>
2019-07-01 22:50:36 +0800

    Selenium Project Initialization

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumPlaying
+{
+    internal class Program
+    {
+        private static readonly IWebDriver WebDriver = new ChromeDriver(@"C:\Users\steze\source\repos\SeleniumPlaying\SeleniumPlaying");
+        private static readonly string BaseUrl = "https://www.youtube.com";
+
+        private static void Main(string[] args)
+        {
+            WebDriver.Navigate().GoToUrl(BaseUrl);
+            WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            var searchInput = WebDriver.FindElement(By.Id("search"));
+            searchInput.SendKeys("Selenium Playing is Fun");
+        }
+    }
+}
